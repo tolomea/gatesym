58e5e234304480d97a18907cf153ab9c997e3029
Gordon Wrigley <gordon.wrigley@gmail.com>
2015-01-12 07:33:49 +0000

    mux planning

--- networks.py
+++ networks.py
@@ -24,6 +24,22 @@ def ripple_adder(aw, bw):
     return rw, c
 
 
+def bit_mux(address, bits):
+    pass
+
+
+def bit_demux(address, data):
+    pass
+
+
+def word_mux(address, words):
+    pass
+
+
+def word_demux(address, data_word):
+    pass
+
+
 def test_half_adder():
     network = Network()
     a = Tie(network)
@@ -143,3 +159,38 @@ if __name__ == "__main__":
     test_half_adder()
     test_full_adder()
     test_ripple_adder()
+
+""" muxes
+    mux n address, 2**n inputs
+
+    flat address
+        for each input we have an And spanning all the address lines
+        2**n * n
+
+    tree address
+        binary tree of pairs of 2 input Ands one tree level per address line
+        except for the top level which isn't necessary
+        2 * (2**(n + 1) - 4)  =  4*(2**n - 2)
+
+    flat demux
+        for each output we have an And spanning all the address lines and the input
+        2**n * (n+1)
+
+    tree demux
+        binary tree of pairs of 2 input Ands one tree level per address line
+        2 * (2**(n + 1) - 2)  =  4*(2**n - 1)
+
+    flat mux
+        for each input we have an And spanning all the address lines and input bit
+        spanning all of the Ands is a huge Or
+        ands(2**n * (n+1)) + ors(2**n)  =  2**n * (n+2)
+
+    tree mux
+        binary tree of pairs of 2 input Ands joined by ors, one tree level per addres line
+        2 * (ands(2**(n+1)-2) + ors(2**n - 1))  =  6*(2**n - 1)
+
+    to increase mux word size we can replicate the whole structure
+    or have a shared address into a 2 input And per bit
+    with an Or across all the bits in the same position
+    for demux we can leave out the or
+"""
