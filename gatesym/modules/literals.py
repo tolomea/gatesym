9765011c8ce27d1a25c8d2bce2bb2ec665e47cf6
Gordon Wrigley <gordon.wrigley@gmail.com>
2015-06-28 18:12:08 +0100

    first cpu module

---
+++ gatesym/modules/literals.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals, division, absolute_import
+
+from gatesym.gates import Tie, block
+
+
+
+@block()
+def low_literal(clock, write, address, data_in):
+    data_out = []
+    data_out.extend(address)
+    for i in range(len(data_in) - len(address)):
+        data_out.append(Tie(clock.network))
+    return data_out
+
+
+@block()
+def high_literal(clock, write, address, data_in):
+    data_out = []
+    for i in range(len(data_in) - len(address)):
+        data_out.append(Tie(clock.network))
+    data_out.extend(address)
+    return data_out
---
+++ gatesym/tests/modules/test_literals.py
@@ -0,0 +1,46 @@
+from __future__ import unicode_literals, division, absolute_import
+
+import random
+
+from gatesym import core, gates, utils
+from gatesym.modules import literals
+
+
+def test_low_literal():
+    network = core.Network()
+    clock = gates.Tie(network)
+    write = gates.Tie(network)
+    data_in = utils.BinaryIn(network, 8)
+    address = utils.BinaryIn(network, 4)
+    low_literal = literals.low_literal(clock, write, address, data_in)
+    data_out = utils.BinaryOut(low_literal)
+
+    # read a value
+    v = random.randrange(16)
+    address.write(v)
+    assert data_out.read() == v
+
+    # and another
+    v = random.randrange(16)
+    address.write(v)
+    assert data_out.read() == v
+
+
+def test_high_literal():
+    network = core.Network()
+    clock = gates.Tie(network)
+    write = gates.Tie(network)
+    data_in = utils.BinaryIn(network, 8)
+    address = utils.BinaryIn(network, 4)
+    low_literal = literals.high_literal(clock, write, address, data_in)
+    data_out = utils.BinaryOut(low_literal)
+
+    # read a value
+    v = random.randrange(16)
+    address.write(v)
+    assert data_out.read() == v << 4
+
+    # and another
+    v = random.randrange(16)
+    address.write(v)
+    assert data_out.read() == v << 4
