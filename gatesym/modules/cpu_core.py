3d5c6b78f78b013657a39ba9231ca2afee1e1cf3
Gordon Wrigley <gordon.wrigley@gmail.com>
2015-07-08 22:28:33 +0100

    cleanup the cpu core a bit to make it more readable

--- gatesym/modules/cpu_core.py
+++ gatesym/modules/cpu_core.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals, division, absolute_import
 
-from gatesym.gates import block, And, Not
+from gatesym.gates import block, And, Or
 from gatesym.blocks.latches import register
 from gatesym.blocks.adders import ripple_incr
-from gatesym.blocks.mux import address_decode, word_switch, word_mux
+from gatesym.blocks.mux import address_decode, word_switch
 from gatesym.utils import PlaceholderWord
 
 
@@ -12,21 +12,26 @@ def cpu_core(clock, data_in):
     network = clock.network
     word_size = len(data_in)
 
+    # step through the 4 states in order
     state = PlaceholderWord(network, 2)
     incr, c = ripple_incr(state)
     state = state.replace(register(incr, clock))
+    s0, s1, s2, s3 = address_decode(state)
 
-    control_lines = address_decode(state)
-
+    # pc increments in s1 and s3
     pc = PlaceholderWord(network, word_size)
     incr, c = ripple_incr(pc)
-    pc = pc.replace(register(incr, And(clock, state[0])))
+    clock_pc = And(clock, Or(s1, s3))
+    pc = pc.replace(register(incr, clock_pc))
 
-    addr = register(data_in, And(clock, Not(state[0])))
-    data_out = register(data_in, And(clock, control_lines[1]))
+    # clock in address in s0 and s2
+    addr = register(data_in, And(clock, Or(s0, s2)))
 
-    addr_out = word_mux(state[:1], pc, addr)
+    # send address as pc in s0 and s2 and addr in s1 and s3
+    addr_out = word_switch([Or(s0, s2), Or(s1, s3)], pc, addr)
 
-    write_out = control_lines[3]
+    # write out data in stage3
+    write_out = s3
+    data_out = register(data_in, And(clock, s1))
 
     return addr_out, data_out, write_out
