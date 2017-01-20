a7400fba5ea86f7c79da6290cb6a9e1dbef3537b
Gordon Wrigley <gordon.wrigley@gmail.com>
2017-01-20 20:08:25 +0000

    refactor computer prior to splitting it

--- gatesym/computer.py
+++ gatesym/computer.py
@@ -6,12 +6,22 @@ from gatesym.modules import bus, cpu_core, math, memory, literals, jump
 from gatesym import core, profiler
 from gatesym.test_utils import BinaryOut
 
+WORD_SIZE = 16
+ROM_BASE = 0x000
+ROM_SIZE = 8
+LIT_BASE = 0x100
+RAM_BASE = 0x200
+RAM_SIZE = 8
+ADD_BASE = 0x300
+SUB_BASE = 0x304
+JUMP_BASE = 0x308
+PRINT_BASE = 0x30b
+
 
 def computer(clock, rom_content):
-    word_size = 16
     network = clock.network
-    data_in = PlaceholderWord(network, word_size)
-    pc_in = PlaceholderWord(network, word_size)
+    data_in = PlaceholderWord(network, WORD_SIZE)
+    pc_in = PlaceholderWord(network, WORD_SIZE)
     pc_write = Placeholder(network)
 
     address, data_out, write_out = cpu_core.cpu_core(clock, data_in, pc_in, pc_write)
@@ -30,7 +40,7 @@ def computer(clock, rom_content):
 
     # lit
     low_literal_write = Placeholder(network)
-    low_literal_data = literals.low_literal(clock, low_literal_write, address, data_out, word_size // 2)
+    low_literal_data = literals.low_literal(clock, low_literal_write, address, data_out, WORD_SIZE // 2)
 
     # print
     print_write = Placeholder(network)
@@ -48,7 +58,7 @@ def computer(clock, rom_content):
 
     modules = [
         (ROM_BASE, ROM_SIZE, rom_data),
-        (LIT_BASE, word_size // 2, low_literal_data),
+        (LIT_BASE, WORD_SIZE // 2, low_literal_data),
         (RAM_BASE, RAM_SIZE, ram_data),
         (ADD_BASE, 2, add_data),
         (SUB_BASE, 2, sub_data),
@@ -70,127 +80,117 @@ def computer(clock, rom_content):
     return print_write, print_data
 
 
-ROM_BASE = 0x000
-ROM_SIZE = 8
-LIT_BASE = 0x100
-RAM_BASE = 0x200
-RAM_SIZE = 8
-ADD_BASE = 0x300
-SUB_BASE = 0x304
-JUMP_BASE = 0x308
-PRINT_BASE = 0x30b
-
-ADD_A = ADD_BASE
-ADD_B = ADD_BASE + 1
-ADD_R = ADD_BASE + 2
-ADD_C = ADD_BASE + 3
-SUB_A = SUB_BASE
-SUB_B = SUB_BASE + 1
-SUB_R = SUB_BASE + 2
-SUB_C = SUB_BASE + 3
-PRINT = PRINT_BASE
-JUMP = JUMP_BASE
-JUMP_DEST = JUMP_BASE + 1
-JUMP_ZERO = JUMP_BASE + 2
-
-
-def LIT(x):
-    return LIT_BASE + x
+symbols = dict(
+    ADD_A=ADD_BASE,
+    ADD_B=ADD_BASE + 1,
+    ADD_R=ADD_BASE + 2,
+    ADD_C=ADD_BASE + 3,
+    SUB_A=SUB_BASE,
+    SUB_B=SUB_BASE + 1,
+    SUB_R=SUB_BASE + 2,
+    SUB_C=SUB_BASE + 3,
+    PRINT=PRINT_BASE,
+    JUMP=JUMP_BASE,
+    JUMP_DEST=JUMP_BASE + 1,
+    JUMP_ZERO=JUMP_BASE + 2,
+    _LIT=LIT_BASE,
+    _RAM=RAM_BASE,
+)
 
 
 def basic_add():
-    return [
-        LIT(123), ADD_A,
-        LIT(5), ADD_B,
-        ADD_R, ADD_A,
-        LIT(67), ADD_B,
-        ADD_R, PRINT,
-    ]
+    return assemble(symbols, [], [
+        123, "ADD_A",
+        5, "ADD_B",
+        "ADD_R", "ADD_A",
+        67, "ADD_B",
+        "ADD_R", "PRINT",
+    ])
 
 
 def loop():
-    i = RAM_BASE
-    return [
-        LIT(1), ADD_B,
-        ADD_R, ADD_A,
-        i, ADD_B,
-        ADD_R, i,
-        i, PRINT,
-        LIT(0), JUMP,
-    ]
+    return assemble(symbols, ["i"], [
+        1, "ADD_B",
+        "ADD_R", "ADD_A",
+        "i", "ADD_B",
+        "ADD_R", "i",
+        "i", "PRINT",
+        0, "JUMP",
+    ])
 
 
 def fib():
-    return assemble([
-        LIT(0), ADD_A,
-        LIT(1), ADD_B,
-        LIT(1), PRINT,
+    return assemble(symbols, [], [
+        0, "ADD_A",
+        1, "ADD_B",
+        1, "PRINT",
         "loop:",
-        ADD_R, PRINT,
-        ADD_R, ADD_A,
-        ADD_R, PRINT,
-        ADD_R, ADD_B,
-        "loop", JUMP,
+        "ADD_R", "PRINT",
+        "ADD_R", "ADD_A",
+        "ADD_R", "PRINT",
+        "ADD_R", "ADD_B",
+        "loop", "JUMP",
     ])
 
 
 def primes():
-    i = RAM_BASE
-    j = RAM_BASE + 1
-    tmp = RAM_BASE + 2
-
-    return assemble([
-        LIT(3), i,
+    return assemble(symbols, ["i", "j", "tmp"], [
+        3, "i",
         "start:",
-        LIT(3), j,
+        3, "j",
         "loop_start:",
-        i, SUB_A,
-        j, SUB_B,
-        "loop_else", JUMP_DEST,
-        SUB_R, JUMP_ZERO,
+        "i", "SUB_A",
+        "j", "SUB_B",
+        "loop_else", "JUMP_DEST",
+        "SUB_R", "JUMP_ZERO",
 
-        i, tmp,
-        j, SUB_B,
+        "i", "tmp",
+        "j", "SUB_B",
         "mod_loop:",
-        tmp, SUB_A,
-        SUB_R, tmp,
-        "loop_end", JUMP_DEST,
-        SUB_A, JUMP_ZERO,
-        "mod_loop", JUMP_DEST,
-        SUB_C, JUMP_ZERO,
+        "tmp", "SUB_A",
+        "SUB_R", "tmp",
+        "loop_end", "JUMP_DEST",
+        "SUB_A", "JUMP_ZERO",
+        "mod_loop", "JUMP_DEST",
+        "SUB_C", "JUMP_ZERO",
 
-        LIT(2), ADD_A,
-        j, ADD_B,
-        ADD_R, j,
+        2, "ADD_A",
+        "j", "ADD_B",
+        "ADD_R", "j",
 
-        "loop_start", JUMP,
+        "loop_start", "JUMP",
         "loop_else:",
-        i, PRINT,
+        "i", "PRINT",
         "loop_end:",
-        LIT(2), ADD_A,
-        i, ADD_B,
-        ADD_R, i,
+        2, "ADD_A",
+        "i", "ADD_B",
+        "ADD_R", "i",
 
-        "start", JUMP,
+        "start", "JUMP",
     ])
 
 
-def assemble(code):
-    labels = {}
+def assemble(symbols, variables, code):
+    symbols = dict(symbols)
+    for offset, name in enumerate(variables):
+        symbols[name] = symbols["_RAM"] + offset
+
+    # collect jump labels
     i = 0
     for c in code:
         if isinstance(c, str) and c.endswith(":"):
-            labels[c[:-1]] = i
+            symbols[c[:-1]] = symbols["_LIT"] + i
         else:
             i += 1
 
+    # convert symbols and int's to addresses
     res = []
     for c in code:
         if isinstance(c, str):
             if not c.endswith(":"):
-                res.append(LIT_BASE + labels[c])
+                res.append(symbols[c])
         else:
-            res.append(c)
+            res.append(symbols["_LIT"] + c)
     return res
 
 
